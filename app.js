ac4467418cd61ad665401a7e8f7259c741c686a3
Evgeniy <Evgeniy.Taburyanskiy@gmail.com>
2019-03-20 20:37:13 +0300

    V 1.0.0

--- app.js
+++ app.js
@@ -33,9 +33,11 @@ const
     oLogger    = require(sUtilsRoot + 'winston')(module),
     oMenu      = require(sUtilsRoot + 'menu');
 
-process.env.NODE_ENV = (oConfig.get('debug')) ? 'development' : '';
+require('dotenv').config();
 
-process.title = 'StakeHolder Overload Tests';
+//process.env.NODE_ENV = (oConfig.get('debug')) ? 'development' : 'production';
+
+process.title = 'Minter Overload Tests';
 process.on('uncaughtException', function(err) {
   oLogger.error('Caught exception: ' + err);
   console.log(err.stack.split('\n'));
@@ -50,112 +52,124 @@ oUtils.showAppCliTitle();
 
 // ==============================================================================
 
-oMinterHelper.init().then(async () => {
+oMinterHelper.init()
+    .then(async () => {
 
-  const
-      fSendFee           = 0.02,
-      fDelegateFee       = 0.1,
-      fTxAmount          = 0.1,
-      iTotalTestDuration = parseInt(oConfig.get('totalTestDuration')) || 60,// seconds
-      iTotalTxPerWallet  = Math.round(iTotalTestDuration / 5),
-      iTotalWalletsCount = parseInt(oConfig.get('totalWallets')) || 10;// workers
+      const
+          fSendFee           = 0.1,
+          fDelegateFee       = 0.2,
+          fTxAmount          = 0.1,
+          iTotalTestDuration = parseInt(oConfig.get('totalTestDuration')) || 60,// seconds
+          iTotalTxPerWallet  = Math.round(iTotalTestDuration / 5),
+          iTotalWalletsCount = parseInt(oConfig.get('totalWallets')) || 10;// workers
 
-  // prepare Wallets
-  let arWalletInstances = await oMinterHelper.loadWallets(iTotalWalletsCount);
+      while (true) {
 
-  if (!arWalletInstances.length) {
-    arWalletInstances = await oMinterHelper.createWallets(iTotalWalletsCount);
-    await  oMinterHelper.saveWallets(arWalletInstances);
-  } else if (arWalletInstances.length < iTotalWalletsCount) {
-    let iDiff = iTotalWalletsCount - arWalletInstances.length;
-    arWalletInstances = arWalletInstances.concat(await oMinterHelper.createWallets(iDiff));
-    await  oMinterHelper.saveWallets(arWalletInstances);
-  }
+        // prepare Wallets
+        let arWalletInstances = await oMinterHelper.loadWallets(iTotalWalletsCount);
 
-  if (arWalletInstances.length) {
-
-    let oAnswer = await oMenu.askTestType();
-
-    switch (oAnswer.sTestType) {
-        /*
-        * Тест Опустошения кошельков.
-        * Выводит деньги со всех кошельков на главный кошелек
-        * Отправляет запросы пачками по наростающей 5,10,15,20...
-        */
-      case 'withdrawal':
-        try {
-          const WithdrawalTest = require(sTestsRoot + 'withdrawal');
-          await WithdrawalTest.run(oMinterHelper, {
-            arWallets: arWalletInstances,
-            fSendFee : fSendFee
-          });
+        if (!arWalletInstances.length) {
+          arWalletInstances = await oMinterHelper.createWallets(iTotalWalletsCount);
+          await oMinterHelper.saveWallets(arWalletInstances);
+        } else if (arWalletInstances.length < iTotalWalletsCount) {
+          let iDiff = iTotalWalletsCount - arWalletInstances.length;
+          arWalletInstances = arWalletInstances.concat(await oMinterHelper.createWallets(iDiff));
+          await oMinterHelper.saveWallets(arWalletInstances);
         }
-        catch (err) {
-          oLogger.error(`Failed withdrawal: Err ${err.message}`);
-          process.exit(1);
-        }
-        break;
-        /*
-        * Тест Пополнения кошельков
-        * Генерит Чеки с главного кошелька, на сумму достаточную для теста Send, и обналичивает их.
-        * Обналичивание производится по схеме : за раз параллельно 1/10 от общего кол-ва кошельков
-        */
-      case 'fund':
-        try {
-          const FundTest = require(sTestsRoot + 'fund_check');
-          await FundTest.run(oMinterHelper, {
-                arWallets     : arWalletInstances,
-                fSendFee      : fSendFee,
-                fFundPerWallet: (fTxAmount + fDelegateFee) * iTotalTxPerWallet
+
+        if (arWalletInstances.length) {
+
+          let oAnswer = await oMenu.askTestType();
+          oUtils.clear();
+
+          switch (oAnswer.sTestType) {
+              /*
+              * Тест Опустошения кошельков.
+              * Выводит деньги со всех кошельков на главный кошелек
+              * Отправляет запросы пачками по наростающей 5,10,15,20...
+              */
+            case 'withdrawal':
+              try {
+                const WithdrawalTest = require(sTestsRoot + 'withdrawal');
+                await WithdrawalTest.run(oMinterHelper, {
+                  arWallets: arWalletInstances,
+                  fSendFee : fSendFee
+                });
               }
-          );
+              catch (err) {
+                oLogger.error(`Failed withdrawal: Err ${err.message}`);
+                process.exit(1);
+              }
+              break;
+              /*
+              * Тест Пополнения кошельков
+              * Генерит Чеки с главного кошелька, на сумму достаточную для теста Send, и обналичивает их.
+              * Обналичивание производится по схеме : за раз параллельно 1/10 от общего кол-ва кошельков
+              */
+            case 'fund':
+              try {
+                const FundTest = require(sTestsRoot + 'fund_check');
+                await FundTest.run(oMinterHelper, {
+                      arWallets     : arWalletInstances,
+                      fSendFee      : fSendFee,
+                      fFundPerWallet: (fTxAmount + fDelegateFee) * iTotalTxPerWallet
+                    }
+                );
+              }
+              catch (err) {
+                oLogger.error(`Failed Async Funding: ${err.message}`);
+                process.exit(1);
+              }
+              break;
+              /*
+              * Тест Отправки монет
+              * Отправляет переводы со ВСЕХ кошельков паралельно. Делает iTotalTxPerWallet итераций.
+              */
+            case 'send':
+              try {
+                const SendTest = require(sTestsRoot + 'send');
+                await SendTest.run(oMinterHelper, {
+                  arWallets        : arWalletInstances,
+                  fSendFee         : fSendFee,
+                  fSendAmount      : fTxAmount,
+                  iTotalTxPerWallet: iTotalTxPerWallet
+                });
+              }
+              catch (err) {
+                oLogger.error(`Failed Send Test: ${err.message}`);
+                process.exit(1);
+              }
+              break;
+              /*
+              * Тест Делегирования
+              * Отправляет запросы пачками по наростающей 5,10,15,20...
+              */
+            case 'delegate':
+              try {
+                const DelegateTest = require(sTestsRoot + 'delegate');
+                await DelegateTest.run(oMinterHelper, {
+                  arWallets      : arWalletInstances,
+                  fDelegateFee   : fDelegateFee,
+                  fDelegateAmount: fTxAmount
+                });
+              }
+              catch (err) {
+                oLogger.error(`Failed Delegating Test: ${err.message}`);
+                process.exit(1);
+              }
+              break;
+              //
+            case 'exit':
+              process.exit(0);
+              break;
+              //
+            default:
+              break;
+          }
+
+
         }
-        catch (err) {
-          oLogger.error(`Failed Async Funding: ${err.message}`);
-          process.exit(1);
-        }
-        break;
-        /*
-        * Тест Отправки монет
-        * Отправляет переводы со ВСЕХ кошельков паралельно. Делает iTotalTxPerWallet итераций.
-        */
-      case 'send':
-        try {
-          const SendTest = require(sTestsRoot + 'send');
-          await SendTest.run(oMinterHelper, {
-            arWallets        : arWalletInstances,
-            fSendFee         : fSendFee,
-            fSendAmount      : fTxAmount,
-            iTotalTxPerWallet: iTotalTxPerWallet
-          });
-        }
-        catch (err) {
-          oLogger.error(`Failed Send Test: ${err.message}`);
-          process.exit(1);
-        }
-        break;
-        /*
-        * Тест Делегирования
-        * Отправляет запросы пачками по наростающей 5,10,15,20...
-        */
-      case 'delegate':
-        try {
-          const DelegateTest = require(sTestsRoot + 'delegate');
-          await DelegateTest.run(oMinterHelper, {
-            arWallets      : arWalletInstances,
-            fDelegateFee   : fDelegateFee,
-            fDelegateAmount: fTxAmount
-          });
-        }
-        catch (err) {
-          oLogger.error(`Failed Delegating Test: ${err.message}`);
-          process.exit(1);
-        }
-        break;
-        //
-      default:
-        break;
-    }
-  }
-});
+      }
+
+    });
 
